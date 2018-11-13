e90ff43464a15af8bfd448bce744679c02e3fa53
ShotaOda <shota.oda@bizreach.co.jp>
2018-11-13 09:16:03 +0900

    fix feature envy

--- carbon-validation/src/main/kotlin/org/carbon/objects/validation/Validator.kt
+++ carbon-validation/src/main/kotlin/org/carbon/objects/validation/Validator.kt
@@ -9,10 +9,7 @@ object Validator {
     fun <T : Validated<T>> validate(test: T): Evaluation {
         val assert = Assertion()
         test.def(assert, test)
-        return when {
-            assert.isValid() -> Evaluation.Acceptance
-            else -> assert.rejection
-        }
+        return assert.evaluation
     }
 }
 
