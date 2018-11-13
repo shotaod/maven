98145f36180362592e79261965fe0b1c9606aafe
ShotaOda <shota.oda@bizreach.co.jp>
2018-11-13 09:15:17 +0900

    remove unused extend function

--- carbon-validation/src/main/kotlin/org/carbon/objects/validation/matcher/MatcherHelper.kt
+++ carbon-validation/src/main/kotlin/org/carbon/objects/validation/matcher/MatcherHelper.kt
@@ -1,9 +1,7 @@
 package org.carbon.objects.validation.matcher
 
-import org.carbon.objects.validation.Logical
 import org.carbon.objects.validation.evaluation.Evaluation
 import org.carbon.objects.validation.evaluation.Key
-import org.carbon.objects.validation.evaluation.rejection.CompositeRejection
 import org.carbon.objects.validation.evaluation.rejection.UnitRejection
 import org.carbon.objects.validation.evaluation.source.Code
 import org.carbon.objects.validation.evaluation.source.ParamList
@@ -13,17 +11,9 @@ fun <T : Any> T.reject(
         code: Code,
         params: ParamList<*>,
         defaultMessage: String? = ""
-): Evaluation.Rejection<T> = UnitRejection(
-        Key.ShouldBeResolved,
-        this,
-        Source(code, params, defaultMessage)
-)
-
-fun <T : Any> T.reject(
-        vararg rejection: Evaluation.Rejection<T>,
-        logical: Logical
-): Evaluation.Rejection<T> = CompositeRejection(
-        this,
-        logical,
-        rejection.toList()
-)
+): Evaluation.Rejected = Evaluation.Rejected.from(
+        UnitRejection(
+                Key.Undefined,
+                this,
+                Source(code, params, defaultMessage)
+        ))
