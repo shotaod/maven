e964b831018c07de47d069ac99fc45d905c251c4
ShotaOda <shota.oda+github@bizreach.co.jp>
2019-01-23 01:37:29 +0900

    format & unused kotlintest dependency

--- carbon-validation/src/test/kotlin/org/carbon/objects/validation/JsonSerializeTest.kt
+++ carbon-validation/src/test/kotlin/org/carbon/objects/validation/JsonSerializeTest.kt
@@ -5,14 +5,13 @@ import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.ser.std.StdDelegatingSerializer
 import com.fasterxml.jackson.databind.util.StdConverter
 import io.kotlintest.matchers.types.shouldBeTypeOf
-import io.kotlintest.specs.AnnotationSpec
 import org.carbon.objects.validation.evaluation.Evaluation
 import org.carbon.objects.validation.evaluation.Key
 import org.carbon.objects.validation.evaluation.source.Code
 import org.carbon.objects.validation.input.ClassRoomInput
 import org.junit.jupiter.api.Test
 
-class JsonSerializeTest : AnnotationSpec() {
+class JsonSerializeTest {
 
     private val objectMapper = ObjectMapper().apply {
         registerModule(CarbonValidationModule)
@@ -21,22 +20,22 @@ class JsonSerializeTest : AnnotationSpec() {
     @Test
     fun serialize_violation() {
         val validation = ClassRoomInput()
-                .person {
-                    name("s")
-                    password("illegal")
-                    portfolioUrl("not:a:url")
-                }
-                .person {
-                    emails("john@carbon.org", "c@rbon")
-                    onetimePw(45678)
-                }
-                .tryValidate()
+            .person {
+                name("s")
+                password("illegal")
+                portfolioUrl("not:a:url")
+            }
+            .person {
+                emails("john@carbon.org", "c@rbon")
+                onetimePw(45678)
+            }
+            .tryValidate()
         validation.shouldBeTypeOf<Evaluation.Rejected>()
         val rejected = validation as Evaluation.Rejected
 
         val json = objectMapper
-                .writerWithDefaultPrettyPrinter()
-                .writeValueAsString(rejected.entries)
+            .writerWithDefaultPrettyPrinter()
+            .writeValueAsString(rejected.entries)
 
         println(json)
     }
